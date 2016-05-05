5f8ac72e11244c8290bf002059a99938b4a41fb1
ptgc <ptgc@PETTER-LAPTOP>
2016-05-05 18:50:24 +0200

    Add project files.

---
+++ CDUUserhandling/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CDUUserhandling.MvcApplication" Language="C#" %>
---
+++ CDUUserhandling/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CDUUserhandling.Startup))]
+namespace CDUUserhandling
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
